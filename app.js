2769dba5700fc8f3612d2fa81940f2a956786035
haas36u <haasmyriam@yahoo.com>
2017-10-11 17:57:22 +0200

    Add menu + triggerAction, reloadAction, cancelAction

--- app.js
+++ app.js
@@ -16,9 +16,34 @@ server.post('/api/messages', connector.listen());
 
 var bot = new builder.UniversalBot(connector, [
     function(session){
-        session.beginDialog('greatings');
+       // session.beginDialog('greatings');
+        session.send('Bienvenue dans le bot de réservation');
+        session.beginDialog('mainMenu');
     }
 ]);
+
+// Main menu
+var menuItems = { 
+    "Saluer": {
+        item: "askName"
+    },
+    "Effectuer une réservation": {
+        item: "reservation"
+    },
+}
+
+bot.dialog("mainMenu", [
+    function(session){
+        builder.Prompts.choice(session, "Menu :", menuItems, { listStyle: builder.ListStyle.list });
+    },
+    function(session, results){
+        if(results.response)    session.beginDialog(menuItems[results.response.entity].item);
+    }
+])
+.triggerAction({
+    matches: /^main menu$/i,
+    confirmPrompt: "Cela annulera votre demande. Etez-vous sûr ?"
+});
 
 bot.dialog('greatings', [
     function(session){
@@ -36,9 +61,24 @@ bot.dialog('askName', [
         builder.Prompts.text(session, 'Comment vous appelez vous ?');
     },
     function(session, results){
+        session.send(`Salut ${results.response}!`);
         session.endDialogWithResult(results);
     }
-]);
+])
+.reloadAction(
+    "restartAskName", "Ok. Nous allons recommencer.",
+    {
+        matches: /^start over$/i,
+        confirmPrompt: "Cela annulera l'action. Etez-vous sûr ?"
+    }
+)
+.cancelAction(
+    "cancelReservation", "Ecrivez 'main menu' pour continuer.", 
+    {
+        matches: /^cancel$/i,
+        confirmPrompt: "Cela annulera l'action. Etez-vous sûr ?"
+    }
+);
 
 bot.dialog('reservation', [
     function(session){
@@ -56,7 +96,21 @@ bot.dialog('reservation', [
     function(session){
         session.send(`Je récapitule : <br> Une réservation le ${session.privateConversationData.reservationDate}, <br>pour une table de ${session.privateConversationData.reservationNbr}, <br>au nom de ${session.privateConversationData.reservationName} <br> et nous pouvons vous contacter au ${session.privateConversationData.reservationPhone}`);
     }
-]);
+])
+.reloadAction(
+    "restartReservation", "Ok. Nous allons recommencer.",
+    {
+        matches: /^start over$/i,
+        confirmPrompt: "Cela annulera votre commande. Etez-vous sûr ?"
+    }
+)
+.cancelAction(
+    "cancelReservation", "Ecrivez 'main menu' pour continuer.", 
+    {
+        matches: /^cancel$/i,
+        confirmPrompt: "Cela annulera votre commande. Etez-vous sûr ?"
+    }
+);
 
 bot.dialog('reservationDate', [
     function(session){
@@ -97,7 +151,10 @@ bot.dialog('reservationPhone', [
     function(session, results) {
         var number = results.response;
 
-        if(number.toString().length == 10)  session.privateConversationData.reservationPhone = number;
-        else                                session.replaceDialog('reservationPhone', { reprompt: true });
+        if(number.toString().length == 10) {
+            session.privateConversationData.reservationPhone = number;
+            session.endDialog();
+        }
+        else    session.replaceDialog('reservationPhone', { reprompt: true });
     }
 ]);
