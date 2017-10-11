7733f374f957c632e6b3b3856f873e46ec55a983
haas36u <haasmyriam@yahoo.com>
2017-10-11 10:39:45 +0200

    First commit : Bot ask your name + say hello userName

--- app.js
+++ app.js
@@ -14,33 +14,26 @@ var connector = new builder.ChatConnector({
 
 server.post('/api/messages', connector.listen());
 
-var bot = new builder.UniversalBot(connector, function(session){
-    // Check if user is writing something
-    bot.on('typing', function(){
-        session.send(`haha, t'es en train d'écrire`);
-    });
-
-    // doheavywork, wait 10 sec before continue next message
-    if(session.message.text === "doheavywork"){
-        session.sendTyping();
-        session.delay(10000);
-        session.send(`I just finished this hard work`);
-    } else { // else write some informations
-        session.send(`OK, ça fonctionne  !! | [Message.length :  ${session.message.text.length}]`);
-        session.send(`DialogData = ${JSON.stringify(session.dialogData)}`);
-        session.send(`Session = ${JSON.stringify(session.sessionState)}`);
+var bot = new builder.UniversalBot(connector, [
+    function(session){
+        session.beginDialog('greatings');
     }
-});
+]);
 
-// Send greating message
-bot.on('conversationUpdate', message => {
-    if (message.membersAdded) {
-        var memberName = message.membersAdded.map(function (data) {
-            if (data.id !== message.address.bot.id){ // Check if added members is a bot or not
-                bot.send(new builder.Message()
-                    .address(message.address)
-                    .text(`Hello ${data.name}`));
-            }
-        });
+bot.dialog('greatings', [
+    function (session) {
+        session.beginDialog('askName');
+    },
+    function (session, results) {
+        session.send(`Hello ${results.response}!`);
     }
-})
+]);
+
+bot.dialog('askName', [
+    function (session){
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.endDialogWithResult(results);
+    }
+]);
