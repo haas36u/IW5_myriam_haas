089e31084d72dbf2f860e28e245aa5f3d5286e90
haas36u <haasmyriam@yahoo.com>
2017-10-11 12:15:05 +0200

    Bot reservation : ask date, nbr people, reservation name and display recap

--- app.js
+++ app.js
@@ -21,19 +21,67 @@ var bot = new builder.UniversalBot(connector, [
 ]);
 
 bot.dialog('greatings', [
-    function (session) {
+    function(session){
+        session.send('Bienvenue dans le bot de réservation');
         session.beginDialog('askName');
     },
-    function (session, results) {
-        session.send(`Hello ${results.response}!`);
+    function(session, results){
+        session.send(`Salut ${results.response}!`);
+        session.beginDialog('reservation');
     }
 ]);
 
 bot.dialog('askName', [
-    function (session){
-        builder.Prompts.text(session, 'Hi! What is your name?');
+    function(session){
+        builder.Prompts.text(session, 'Comment vous appelez vous ?');
     },
-    function (session, results) {
+    function(session, results){
         session.endDialogWithResult(results);
+    }
+]);
+
+bot.dialog('reservation', [
+    function(session){
+        session.beginDialog('reservationDate');
+    },
+    function(session){
+        session.beginDialog('reservationNbr');
+    },
+    function(session){
+        session.beginDialog('reservationName');
+    },
+    function(session){
+        session.send(`Je récapitule, une réservation le ${session.privateConversationData.reservationDate}, pour une table de ${session.privateConversationData.reservationNbr}, au nom de  ${session.privateConversationData.reservationName}`);
+    }
+]);
+
+bot.dialog('reservationDate', [
+    function(session){
+        builder.Prompts.time(session, 'A quelle date voulez-vous réserver ?');
+    },
+    function(session, results){
+        var reservationDate = builder.EntityRecognizer.resolveTime([results.response]);
+        session.privateConversationData.reservationDate = reservationDate.toLocaleDateString('fr');
+        session.endDialog('J\'ai pris note');
+    }
+]);
+
+bot.dialog('reservationNbr', [
+    function(session){
+        builder.Prompts.number(session, 'Une table pour combien de personnes ?');
+    },
+    function(session, results){
+        session.privateConversationData.reservationNbr = results.response;
+        session.endDialog('D\'accord');
+    }
+]);
+
+bot.dialog('reservationName', [
+    function(session){
+        builder.Prompts.text(session, 'A quel nom est la réservation ?');
+    },
+    function(session, results){
+        session.privateConversationData.reservationName = results.response;
+        session.endDialog('C\'est noté');
     }
 ]);
